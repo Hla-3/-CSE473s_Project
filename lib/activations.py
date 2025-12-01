073eff79ee403cce1d50095859c99cf7a79a38cf
Youssef Ahmed <youssef.nagy073@gmail.com>
2025-12-01 22:49:16 +0200

    initial loss and activation

---
+++ lib/activations.py
@@ -0,0 +1,7 @@
+def Activate(x,type):
+    #Here we calculate activateion function of one of four methods:
+    #1-ReLU
+    #2-Sigmoid
+    #3-Tanh
+    #4-Softmax
+    pass
---
+++ lib/losses.py
@@ -0,0 +1,3 @@
+def MSE(y_true,y_pred):
+    #Here we calculate the loss function using Mean Squared Error method
+    pass
